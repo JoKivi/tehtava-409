4bd4a86f0c3014e2d7b4eec2eb2de034329c3133
Joni Kivimaki <joni.kivimaki@elisanet.fi>
2022-05-16 11:04:53 +0300

    Väli

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const valitutDesimaalit = document.querySelectorAll("input[name='valittu']");
+let tulo;
+
 const alarajaCelsius = -273.15;
 const alarajaKelvin = 0;
 const alarajaFahrenheit = -459.67;
@@ -10,9 +12,6 @@ for (const valittuDesimaali of valitutDesimaalit) {
         break;
     }
 }
-// if (valittuKoko === ) {
-    
-// }
 
 
 document.getElementById("button").addEventListener("click", muunna);
@@ -29,8 +28,50 @@ function muunna(event) {
             if (syote < alarajaCelsius) {
                 alerting();
             }
+            tulo = syote*1.8+32;
+            tuloste();
             break;
-    
+
+        case 'F-C':
+            if (syote < alarajaFahrenheit) {
+                alerting();
+            }
+            tulo = (syote-32)/1.8;
+            tuloste();
+            break;
+
+        case 'C-K':
+            if (syote < alarajaCelsius) {
+                alerting();
+            }
+            tulo = syote+273.15;
+            tuloste();
+            break; 
+        
+        case 'K-C':
+            if (syote < alarajaKelvin) {
+                alerting();
+            }
+            tulo = syote-273.15;
+            tuloste();
+            break;
+
+        case 'F-K':
+            if (syote < alarajaFahrenheit) {
+                alerting();
+            }
+            tulo = (syote+459.67)*(5/9);
+            tuloste();
+            break; 
+
+        case 'K-F':
+            if (syote < alarajaKelvin) {
+                alerting();
+            }
+            tulo = 1.8*(syote-273.15)+32;
+            tuloste();
+            break;
+
         default:
             break;
     }
@@ -40,5 +81,13 @@ function muunna(event) {
 
 function alerting() {
     alert("Arvo on alle absoluuttisen nollapisteen!!! Et voi mennä sen alle.");
+}
+
+function tuloste() {
+    if (valittuKoko) {
+        
+    }
+    
+    document.getElementById("vastausalue").innerHTML = tulo;
 }
 
